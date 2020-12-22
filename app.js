0047a3e3eae8450e61f615e1a5e8fd74d2bde245
Ivan Pli <plisyakov.ivan@mail.ru>
2020-12-22 15:57:48 +0700

    12.0.0

---
+++ app.js
@@ -0,0 +1,23 @@
+/*eslint no-const-assign: "error"*/
+/*eslint-env es6*/
+const express = require('express');
+
+const routerCards = require('./routes/cards.js');
+const routerUsers = require('./routes/users.js');
+const path = require('path');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routerCards);
+app.use('/', routerUsers)
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
+
