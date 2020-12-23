3200f540500ca5ff20ad8fac7dac89515ae56c4b
Ivan Pli <plisyakov.ivan@mail.ru>
2020-12-23 21:36:20 +0700

    12.2.0

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-/*eslint no-const-assign: "error"*/
-/*eslint-env es6*/
 const express = require('express');
 
+const path = require('path');
 const routerCards = require('./routes/cards.js');
 const routerUsers = require('./routes/users.js');
-const path = require('path');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routerCards);
-app.use('/', routerUsers)
-
+app.use('/', routerUsers);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
-
+  console.log(`App listening on port ${PORT}`);
+});
