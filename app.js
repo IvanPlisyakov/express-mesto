cbedf41943adc8b304da04b0730a5d0cf0504640
Ivan Pli <plisyakov.ivan@mail.ru>
2021-01-27 23:47:34 +0700

    15.2.6

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://spora.students.nomoredomains.monster');
+  res.header('Access-Control-Allow-Origin: *');
+  res.header('Access-Control-Allow-Methods: *');
+  res.header('Access-Control-Allow-Headers', 'origin, content-type, accept');
 
   next();
 });
