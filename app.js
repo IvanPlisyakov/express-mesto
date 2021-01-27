81da927ba511719a61f2fb35d5e1a097eaebf6d8
Ivan Pli <plisyakov.ivan@mail.ru>
2021-01-27 18:59:11 +0700

    15.2.2

--- app.js
+++ app.js
@@ -23,10 +23,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.get('/crash-test', () => {
