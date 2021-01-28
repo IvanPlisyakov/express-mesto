79a609b54f435a32f76f310212e10f8204138ea8
Ivan Pli <plisyakov.ivan@mail.ru>
2021-01-28 19:46:04 +0700

    15.3.3

--- routes/cards.js
+++ routes/cards.js
@@ -7,7 +7,7 @@ const {
 router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).min(30),
+    name: Joi.string().required().min(2),
     link: Joi.string().required().min(2),
   }),
 }),createCard);
