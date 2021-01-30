72399d43c446cf2c2d37b01b177c5ef8c6e9b7f4
Ivan Pli <plisyakov.ivan@mail.ru>
2021-01-30 21:17:04 +0700

    15.5.8

--- routes/cards.js
+++ routes/cards.js
@@ -8,7 +8,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2),
-    link: Joi.string().required().min(2),
+    link: Joi.string().required().min(2).regex(/https?:\/\/w{0,3}[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*#?/),
   }),
 }),createCard);
 router.delete('/:id', celebrate({
@@ -18,12 +18,12 @@ router.delete('/:id', celebrate({
 }),deleteCard);
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().min(2),
+    cardId: Joi.string().required().min(24).max(24).hex(),
   }).unknown(true),
 }),addLikeCard);
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().min(2),
+    cardId: Joi.string().required().min(24).max(24).hex(),
   }).unknown(true),
 }),removeLikeCard);
 
