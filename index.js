de20131618d152463402362b95a4c0da958ee0ba
Ivan Pli <plisyakov.ivan@mail.ru>
2021-12-14 01:58:27 +0300

    11

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ app.use((req, res, next) => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });*/
 
-//app.use('/api', routers);
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(routers);
+//app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/signin', express.static(path.join(__dirname, 'build')));
-app.use('/signup', express.static(path.join(__dirname, 'build')));
+//app.use('/signin', express.static(path.join(__dirname, 'build')));
+//app.use('/signup', express.static(path.join(__dirname, 'build')));
 
 
 
